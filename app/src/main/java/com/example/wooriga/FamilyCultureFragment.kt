3640fd41433be3061f0b9d934ff5144509f37d46
Yerim Son <sonyerim12@gmail.com>
2025-05-29 19:51:33 +0900

    [FEAT] 가족 문화 툴바 아이콘 클릭 리스너 (임시)

--- app/src/main/java/com/example/wooriga/FamilyCultureFragment.kt
+++ app/src/main/java/com/example/wooriga/FamilyCultureFragment.kt
@@ -1,12 +1,14 @@
 package com.example.wooriga
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.example.wooriga.databinding.FragmentFamilyCultureBinding
-import com.example.wooriga.databinding.FragmentHomeBinding
 
 class FamilyCultureFragment : Fragment() {
 
@@ -26,6 +28,19 @@ class FamilyCultureFragment : Fragment() {
         _binding = FragmentFamilyCultureBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // include된 툴바에서 아이콘 가져오기
+        val iconMenu = view.findViewById<Toolbar>(R.id.custom_toolbar)
+            .findViewById<ImageView>(R.id.icon_menu)
+
+        iconMenu.setOnClickListener {
+            // 임시 토스트 메세지
+            Toast.makeText(requireContext(), "가족 선택 클릭됨", Toast.LENGTH_SHORT).show()
+    }
     }
 
 }
