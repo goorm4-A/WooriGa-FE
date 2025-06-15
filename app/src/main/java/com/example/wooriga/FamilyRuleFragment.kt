acdcef389577e4e737b56fb8ee9cf95ef00b884f
Yerim Son <sonyerim12@gmail.com>
2025-06-15 17:09:21 +0900

    [FEAT] 규칙 화면 툴바 설정

--- app/src/main/java/com/example/wooriga/FamilyRuleFragment.kt
+++ app/src/main/java/com/example/wooriga/FamilyRuleFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -23,6 +25,18 @@ class FamilyRuleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        // 툴바
+        val toolbar = view.findViewById<View>(R.id.custom_toolbar)
+        val title = toolbar.findViewById<TextView>(R.id.tv_toolbar_title)
+        val btnBack = toolbar.findViewById<ImageButton>(R.id.btn_back)
+
+        title.text = "규칙"
+
+        btnBack.setOnClickListener {
+            requireActivity().findViewById<View>(R.id.bottomNavigation).visibility = View.VISIBLE
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
 
         adapter = RuleListAdapter { rule ->
             RuleDetailBottomSheet(rule).show(parentFragmentManager, "RuleDetailBottomSheet")
