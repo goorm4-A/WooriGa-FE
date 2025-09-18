c043a2e8d594684ddfaaea75c57c6a329b332d06
wonyoungg-10 <cyoung10@sookmyung.ac.kr>
2025-09-19 02:42:02 +0900

    .

---
+++ app/src/main/java/com/example/wooriga/FamilyTreeViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.wooriga
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.io.File
+
+class FamilyMemberViewModel(private val repository: FamilyMemberRepository) : ViewModel() {
+
+}
+
+
